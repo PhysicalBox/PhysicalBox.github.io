5fe5837453c1a18acf4a511e8e9a46efd4940c5f
Sonya <sonya.castro.gomez@gmail.com>
2020-12-17 00:44:01 -0500

    Estilos

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+document.getElementById("modified").innerHTML = document.lastModified;
 window.onscroll = function() { myFunction() };
 
-var nav = document.getElementById("nav");
-var sticky = nav.offsetTop;
+const nav = document.getElementById("nav")
+const sticky = nav.offsetTop;
+var openMenu = true;
 
 function myFunction() {
     if (window.pageYOffset >= sticky) {
@@ -9,4 +11,16 @@ function myFunction() {
     } else {
         nav.classList.remove("sticky");
     }
+}
+
+
+function openNav(elemnt) {
+    elemnt.classList.toggle("spin");
+    nav.classList.add("sticky")
+    if (openMenu)
+        nav.style.display = "block";
+    else
+        nav.style.display = "none";
+
+    openMenu = !openMenu;
 }
